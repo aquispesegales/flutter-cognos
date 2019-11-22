c7cd06abd982504fabe9f0ce152e3f60ff8db0f4
FLUTTER <alvaroquispesegales@gmail.com>
2019-11-21 21:12:09 -0400

    se ha repasado widget

--- flutter_widgets_basicos/lib/main.dart
+++ flutter_widgets_basicos/lib/main.dart
@@ -1,111 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      title: 'Bienvenidos',
+      home:Scaffold(
+        appBar:AppBar(
+          title: Text('Bienvenidos a  flutter'),
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: 
+        //Copiar
+        
+        /*Center(
+          child: Text('Hola Mundo MARCELO'),
+        )*/
+
+        /*Center(
+          child: Container(
+            margin: const EdgeInsets.all(10.0),
+            color:Colors.amber[600],
+            width: 48.0,
+            height: 48.0,
+          ),
+        )*/
+
+        /*Container(
+          constraints: BoxConstraints.expand(
+            height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0
+          ),
+          padding: const EdgeInsets.all(8.0),
+          color: Colors.blue[600],
+          alignment: Alignment.center,
+          child: Text('Hola Munto',
+            style: Theme.of(context)
+            .textTheme
+            .display1
+            .copyWith(color: Colors.white)),
+            transform: Matrix4.rotationZ(0.1),
+          )*/
+
+        /*Row(
+            children: <Widget>[
+              Expanded(
+                child: Text('Texto alineado a la isquierda',
+                textAlign: TextAlign.center)
+              ),
+              Expanded(
+                child: Text('Texto al medio', textAlign:TextAlign.center)
+              ),
+              Expanded(
+                child: FittedBox(
+                  fit: BoxFit.contain,
+                  child: const FlutterLogo(),
+                ),
+              )
+            ],
+          )*/
+
+        /*Row(
+            children: <Widget>[
+              const FlutterLogo(),
+              const Expanded(
+                child: Text('Flutter\'s Desde el corazón de la comunidad FlutterES, nace un nuevo concepto de academia online para ayudar a formar profesionales en nuestro SDK favorito'),
+              ),
+              const Icon(Icons.sentiment_very_satisfied),
+              const Expanded(
+                child: Text('rae tu aplicación a la vida en cuestión de milisegundos con Hot R eload. Utilice un completo conjunto de widgets totalmente personalizables para crear inte rfaces nativas en cuestión de minutos.'),
+              )
+            ],
+          )*/
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Desarrollo rápido'),
+              Text('Estado previo se combina con nu nuevo código'),
+              Text('Cambio del código reciente se incluye pero el estado del apk se escluye'),
+              Text('Cambio reciente de la UI es exlcuido'),
+              Text('La funcionalidad hot reload de flutter te ayuda a rapido y facil ente experimentar'),
+              Text('ejecuta la app desde un editor flutter soportDO')
+            ],
+          )
+
+
+        //COPIAR HASTA AAQUI
+      )
     );
   }
 }
